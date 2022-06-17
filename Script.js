e73dd569f77ff855a8e593b7bfa4dbb9772e7cb5
Jeraldheo <jeyson.marin@unah.hn>
2022-06-16 22:22:35 -0600

    Finished  Assigning turns to players

--- Script.js
+++ Script.js
@@ -1,13 +1,32 @@
+
 window.addEventListener("load",start)
-let cel0
+let turn = 0; //Determine which turn is it. even x's turn, odd 0's turn
+let positions = new Array(9)
+let handler = function(){put_mark(this)}
+
+
 function start()
 {
-    cel0 = document.getElementById("test")
-    cel0.addEventListener("click", put_mark)
+
+    for(let i = 0; i<positions.length; i++)
+    {
+        positions[i]= document.getElementById("cel" + i)
+        positions[i].addEventListener("click",handler)
+    }
+    
+   
 }
 
-function put_mark()
-{
-    cel0.innerText ="X"
-    cel0.removeEventListener()
+function put_mark(cel)
+{ 
+    if(turn%2==0)
+    {
+        cel.innerText = "X"
+    } 
+    else
+    {
+        cel.innerText = "0"
+    }
+    turn = turn + 1
+    cel.removeEventListener("click", handler)
 }
